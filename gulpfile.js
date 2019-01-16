fcd9db435c8ac86d10852cda84a85b149eea62a5
badrat <beau@llcdata.com>
2019-01-15 16:18:41 -0800

    everything

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var gls = require('gulp-live-server');
+
+gulp.task( 'default', [ 'serve' ] );
+gulp.task('serve', function() {
+	//1. run your script as a server
+	var server = gls.new('app.js');
+	server.start();
+
+	//use gulp.watch to trigger server actions(notify, start or stop)
+	gulp.watch(['public/**/*.less', 'views/*.ejs'], function (file) {
+        server.notify.apply(server, [file]);
+        server.notify.bind(server)(file);
+	});
+	
+	gulp.watch('app.js', server.start.bind(server)); //restart my server
+
+	// Note: try wrapping in a function if getting an error like `TypeError: Bad argument at TypeError (native) at ChildProcess.spawn`
+	gulp.watch('app.js', function() {
+		server.start.bind(server)()
+	});
+
+});
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Database.la Boilerplate
+
+```This is a boilerplate for any site hosted with database.la, and is configured to use its CMS```  
+NodeJS --- https://nodejs.org/en/ 
+NPM --- https://www.npmjs.com/get-npm 
+Express --- npm install express-generator -g 
+Gulp --- npm i -g gulp 
+Firebase Account (for user validation)  
+
+Then link gulp.. 
+`npm link gulp`  
+
+Install everything with: 
+`npm i`  
