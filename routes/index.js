ed88775f6d376c29279d88c104922eef2c5aa3fd
badrat <beau@llcdata.com>
2020-01-27 15:34:18 -0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -12,35 +12,21 @@ var knex = require('knex')({
   }
 });
 var prod = process.env.production;
+var moment = require('moment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home', prod: prod });
-  console.log(prod);
-=======
-    dialect: 'mysql',
-    connection: {
-        host: (process.env.sqlhost),
-        user: (process.env.sqluser),
-        password: (process.env.sqlpassword),
-        database: (process.env.sqldatabase)
-    }
-});
-var moment = require('moment');
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
 });
 
 router.get('/route/:slug', function (req, res, next) {
-    var routeslug = req.params.slug;
-    var results;
-    knex('table').select('*').where('slug', routeslug).then(function (resp) {
-        results = resp[0];
-        res.render('template', { title: title, results: results, moment: moment });
-    });
->>>>>>> 8b9c950c0f87ddb56e1a823c3adc2dafc6929288
+  var routeslug = req.params.slug;
+  var results;
+  knex('table').select('*').where('slug', routeslug).then(function (resp) {
+    results = resp[0];
+    res.render('template', { title: title, results: results, moment: moment });
+  });
 });
+var moment = require('moment');
 
 module.exports = router;
